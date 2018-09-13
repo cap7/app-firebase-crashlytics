890c56337bdfd5943dd7b42c0200fd983878cbc9
Christopher <ccardenasp@grupoutp.edu.pe>
2018-09-13 12:02:31 -0500

    add-firebase

--- app/src/main/java/com/pe/devfestlima2018/firebasecrashlytics/MyContactsActivity.java
+++ app/src/main/java/com/pe/devfestlima2018/firebasecrashlytics/MyContactsActivity.java
@@ -1,20 +1,14 @@
 package com.pe.devfestlima2018.firebasecrashlytics;
 
-
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends AppCompatActivity {
+public class MyContactsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_contacts);
     }
 }
-
